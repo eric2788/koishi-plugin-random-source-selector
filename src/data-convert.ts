e42300704e2de5da47c91b60fd2319c641498a83
ericlam <ericlam@hket.com>
2023-11-13 15:40:35 +0800

    logger.warn: 重定向链接 -> 资源

--- src/data-convert.ts
+++ src/data-convert.ts
@@ -7,7 +7,7 @@ const convertMap: { [key in SplitType | string]: (res: AxiosResponse) => any } =
     'resource': (res: AxiosResponse) => res.request.res.responseUrl, 
     'image': (res: AxiosResponse) => {
         // logger warn it deprecated and use resource instead
-        logger.warn('返回数据类型为 图片 已经被废弃，请使用 重定向链接')
+        logger.warn('返回数据类型为 图片 已经被废弃，请使用 资源')
         return res.request.res.responseUrl
     }
 }
